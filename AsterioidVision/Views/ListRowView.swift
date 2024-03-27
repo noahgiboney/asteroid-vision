e6fdeee349d3f403c4c2820391317f8c83438b04
noahgiboney <noahgiboney@gmail.com>
2024-03-26 19:45:44 -0700

    feat: pagination of lists

---
+++ AsterioidVision/Views/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRow.swift
+//  AsterioidVision
+//
+//  Created by Noah Giboney on 3/26/24.
+//
+
+import SwiftUI
+
+struct ListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRow()
+}
